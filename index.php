1bb9bb8d4699eba29ca6d5cf19be90b5e3b5ff2c
LAMER83 <uporovo@mail.ru>
2022-09-06 21:07:39 +0500

    home

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 
-
-
-
-
-<select name="filial" id="filial_id"> <option value="0">Выбирите филиал</option></select>
-
-<select name="pole" id="pole_id"> <option value="0">Выбирите поле</option></select>
-
-<select name="kyltura" id="kyltura_id"> <option value="0">Выбирите культуру</option></select>
-
 <?php
+$optionFilial = '';
+$db = new PDO('sqlite:db\othod.sqlite3');
+$query = $db->prepare("SELECT * FROM filial");
+$query->execute();
+$i = 1;
+$query_pole = $db->prepare("SELECT * FROM pole");
+$query_pole->execute();
+
+$pul = [];
+
+foreach ($filial = $query->fetchAll() as $value => $item){
+    $optionFilial .= "<option value='$i'>".$item['NameFilial'].'</option>';
+    $i++;
+    foreach ($pole = $query_pole->fetchAll() as $value_pole => $g){
+        //$pul[$item['NameFilial']] = [] [$g['NamePole']] ;
+        $pul[$item['NameFilial']] = array($g['NamePole']) ;
+    }
+}
+
+//$query = $db->prepare("SELECT filial.NameFilial, pole.NamePole FROM filial
+//INNER JOIN pole ON filial.id = pole.ID_Filial");
+
+//$query->execute();
+echo '<pre>';
+print_r($pul);
+//print_r($query->fetchAll());
+//INSERT INTO 'pole' ('ID','ID_Filial', 'NamePole'
+//    ) VALUES (null, 3,'Каргинский');
+//INNER JOIN sostoyanie ON life.id_sostoyanie = sostoyanie.id
+//http://xandeadx.ru/blog/javascript/36
+?>
+
+<select name="filial" id="filial_id"> <option value="0">Выбирите филиал</option><?php echo $optionFilial;?></select>
+<select name="pole" id="pole_id"> <option value="0">Выбирите поле</option></select>
+<select name="kyltura" id="kyltura_id"> <option value="0">Выбирите культуру</option></select>
